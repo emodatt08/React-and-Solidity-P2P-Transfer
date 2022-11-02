52d989b3dbb7b04db95b806181908be6fbf93896
Sadat Hillary Kollan <sadat@farmerline.co>
2022-11-02 10:11:35 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# EShop(React/Redux/Node/Typescript/Mailjet/Stripe)
+
+This is a peer to peer payments app project using infura on the goerli testnet to send funds from one Ethereum account to another
+
+## Dependencies
+
+- Install dependencies with npm
+- Use Metamask as a block explorer
+
+<h2 id="screenshots">Screenshots</h2>
+
+<h6> Transaction and Timeline Page </h6>
+
+![](Screenshots/1.png)
+
+<h6> Metamask Transaction </h6>
+
+![](Screenshots/2.png)
+
+<h6> Metamask Confirmation </h6>
+
+![](Screenshots/3.png)
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import styles from './styles/App.module.css';
+import NavBar from './components/Navbar';
+import TransactionForm from './components/transaction/TransactionForm';
+import ActivityCard from './components/activity/ActivityCard'
+function App() {
+  return (
+    <div className={styles.wrapper}>
+       <header>
+          <NavBar />
+       </header>
+      <main className={styles.mainContainer}>
+        <div className={styles.activityContainer}>
+          <ActivityCard />
+        </div>
+        <div className={styles.sideContainer}>
+          <TransactionForm />
+        </div> 
+      </main>
+       
+    </div>
+  );
+}
+
+export default App;
